06558e6dac4855d67ce839c5c7030872941d022e
Jayakumar <arunramu0811@gmail.com>
2022-11-18 11:27:29 +0530

    kumar

--- src/pullianthoppu/pasanga/AreaGayz.java
+++ src/pullianthoppu/pasanga/AreaGayz.java
@@ -11,6 +11,8 @@ public class AreaGayz {
 		System.out.println("madhesh");
 		System.out.println("muthu");
 		System.out.println("barath");
+		System.out.println("mani");
+		System.out.println("udhai");
 	}
 	public static void main(String[] args) {
 		AreaGayz A = new AreaGayz();
